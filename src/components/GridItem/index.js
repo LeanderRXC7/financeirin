2ee49179710decb8579acc0baf35fbaf71dba954
Leander Campos <leanderrxcampos@gmail.com>
2025-01-07 16:37:21 -0300

    Revert "aaaa"
    This reverts commit a03e7b61e5141cfb7269870c5caa5acca9054a1c.

--- src/components/GridItem/index.js
+++ src/components/GridItem/index.js
@@ -1,31 +1,24 @@
 import React from "react";
 import * as C from "./styles";
-
-import {
-  FaRegArrowAltCircleUp,
-  FaRegArrowAltCircleDown,
-  FaTrash,
-} from "react-icons/fa";
+import React from "react";
+import * as C from "./styles";
+import { FaRegArrowAltCircleUp, FaRegArrowAltCircleDown, FaTrash } from "react-icons/fa";
 
 const GridItem = ({ item, onDelete }) => {
   const formatDate = (date) => {
-    if (!date) return ""; // Garantia contra valores inválidos
+    if (!date) return "";
     const [year, month, day] = date.split("-");
-    return {day}/{month}/{year};
+    return `${day}/${month}/${year}`;
   };
 
   return (
     <C.Tr>
       <C.Td>{item.desc}</C.Td>
       <C.Td>{item.amount}</C.Td>
-      <C.Td>{formatDate(item.date)}</C.Td> {/* Exibindo a data */}
+      <C.Td>{formatDate(item.date)}</C.Td>
       <C.Td>{item.category}</C.Td>
       <C.Td alignCenter>
-        {item.expense ? (
-          <FaRegArrowAltCircleDown color="red" />
-        ) : (
-          <FaRegArrowAltCircleUp color="green" />
-        )}
+        {item.expense ? <FaRegArrowAltCircleDown color="red" /> : <FaRegArrowAltCircleUp color="green" />}
       </C.Td>
       <C.Td alignCenter>
         <FaTrash onClick={() => onDelete(item.id)} />
