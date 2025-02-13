b183fa45b0e21397104cd277dca505d04ef158d1
Leander Campos <leanderrxcampos@gmail.com>
2025-02-13 16:04:06 -0300

    Gráfico pizza de despesas

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -6,6 +6,13 @@ import { db, auth } from "../../firebaseConfig";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import CurrencyInput from "react-currency-input-field";
+import { Bar } from "react-chartjs-2";
+import { Pie } from "react-chartjs-2";
+import { faChartPie } from "@fortawesome/free-solid-svg-icons";
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Form = ({ handleAdd, transactionsList, setTransactionsList }) => {
   const [desc, setDesc] = useState("");
@@ -14,21 +21,99 @@ const Form = ({ handleAdd, transactionsList, setTransactionsList }) => {
   const [date, setDate] = useState("");
   const [category, setCategory] = useState("");
   const [errors, setErrors] = useState({});
+  const [showChart, setShowChart] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  const generatePieChartData = () => {
+    const expenseData = transactionsList.filter((item) => item.expense);
+
+    if (expenseData.length === 0) {
+      return null;
+    }
+
+    const groupedData = expenseData.reduce((acc, item) => {
+      acc[item.category] = acc[item.category]
+        ? acc[item.category] + item.amount
+        : item.amount;
+      return acc;
+    }, {});
+
+    return {
+      labels: Object.keys(groupedData),
+      datasets: [
+        {
+          label: "Despesas por Categoria",
+          data: Object.values(groupedData),
+          backgroundColor: [
+            "#003366",
+            "#007BFF",
+            "#00A36C",
+            "#FF5733",
+            "#FFC300",
+            "#C70039",
+            "#900C3F",
+            "#581845",
+          ],
+          borderColor: "#fff",
+          borderWidth: 2,
+        },
+      ],
+    };
+  };
+
+  const generateChartData = () => {
+    const expenseData = transactionsList.filter((item) => item.expense);
+
+    if (expenseData.length === 0) {
+      alert("Nenhuma despesa cadastrada para gerar o gráfico.");
+      return null;
+    }
+
+    const groupedData = expenseData.reduce((acc, item) => {
+      acc[item.category] = acc[item.category]
+        ? acc[item.category] + item.amount
+        : item.amount;
+      return acc;
+    }, {});
+
+    return {
+      labels: Object.keys(groupedData),
+      datasets: [
+        {
+          label: "Despesas por Categoria",
+          data: Object.values(groupedData),
+          backgroundColor: "#003366",
+          borderColor: "#003366",
+          borderWidth: 1,
+        },
+      ],
+    };
+  };
 
   // Categorias separadas por tipo de transação
   const incomeCategories = ["Salário", "Outras Rendas"];
   const expenseCategories = [
-    "Alimentação", "Contas", "Entretenimento", "Transporte", "Moradia",
-    "Vestuário", "Educação", "Saúde", "Outros"
+    "Alimentação",
+    "Contas",
+    "Entretenimento",
+    "Transporte",
+    "Moradia",
+    "Vestuário",
+    "Educação",
+    "Saúde",
+    "Outros",
   ];
 
   const validateForm = () => {
     let newErrors = {};
 
-    if (!desc || desc.length < 3) newErrors.desc = "A descrição deve ter pelo menos 3 caracteres.";
-    if (!amount || parseFloat(amount) <= 0) newErrors.amount = "O valor deve ser maior que zero.";
+    if (!desc || desc.length < 3)
+      newErrors.desc = "A descrição deve ter pelo menos 3 caracteres.";
+    if (!amount || parseFloat(amount) <= 0)
+      newErrors.amount = "O valor deve ser maior que zero.";
     if (!date) newErrors.date = "A data é obrigatória.";
-    if (new Date(date) > new Date()) newErrors.date = "A data não pode ser futura.";
+    if (new Date(date) > new Date())
+      newErrors.date = "A data não pode ser futura.";
     if (!category) newErrors.category = "Selecione uma categoria.";
 
     setErrors(newErrors);
@@ -49,7 +134,10 @@ const Form = ({ handleAdd, transactionsList, setTransactionsList }) => {
 
     const saveTransactionToFirestore = async (transaction) => {
       try {
-        const docRef = await addDoc(collection(db, "Transactions"), transaction);
+        const docRef = await addDoc(
+          collection(db, "Transactions"),
+          transaction
+        );
         console.log("Transação salva com ID:", docRef.id);
         transaction.id = docRef.id;
         handleAdd(transaction);
@@ -86,8 +174,11 @@ const Form = ({ handleAdd, transactionsList, setTransactionsList }) => {
             onValueChange={(value) => setAmount(value)}
             placeholder="R$ 0,00"
             style={{
-              width: "100%", padding: "5px 0.5px", borderRadius: "5px",
-              border: "1px solid #ccc", fontSize: "15px",
+              width: "100%",
+              padding: "5px 0.5px",
+              borderRadius: "5px",
+              border: "1px solid #ccc",
+              fontSize: "15px",
             }}
           />
           {errors.amount && <C.Error>{errors.amount}</C.Error>}
@@ -95,43 +186,54 @@ const Form = ({ handleAdd, transactionsList, setTransactionsList }) => {
 
         <C.InputContent>
           <C.Label>Data</C.Label>
-          <C.Input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+          <C.Input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
           {errors.date && <C.Error>{errors.date}</C.Error>}
-        </C.InputContent>    
+        </C.InputContent>
 
         <C.RadioGroup>
-          <C.Input 
-            type="radio" 
-            id="rIncome" 
-            name="group1" 
+          <C.Input
+            type="radio"
+            id="rIncome"
+            name="group1"
             checked={!isExpense}
             onChange={() => {
               setExpense(false);
               setCategory(""); // Reseta a categoria ao trocar o tipo
-            }} 
+            }}
           />
           <C.Label htmlFor="rIncome">Entrada</C.Label>
-          
-          <C.Input 
-            type="radio" 
-            id="rExpenses" 
-            name="group1" 
+
+          <C.Input
+            type="radio"
+            id="rExpenses"
+            name="group1"
             checked={isExpense}
             onChange={() => {
               setExpense(true);
               setCategory(""); // Reseta a categoria ao trocar o tipo
-            }} 
+            }}
           />
           <C.Label htmlFor="rExpenses">Saída</C.Label>
         </C.RadioGroup>
 
         <C.InputContent>
           <C.Label>Categoria</C.Label>
-          <C.Select value={category} onChange={(e) => setCategory(e.target.value)}>
+          <C.Select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
             <option value="">Selecione uma categoria</option>
-            {(isExpense ? expenseCategories : incomeCategories).map((cat, index) => (
-              <option key={index} value={cat}>{cat}</option>
-            ))}
+            {(isExpense ? expenseCategories : incomeCategories).map(
+              (cat, index) => (
+                <option key={index} value={cat}>
+                  {cat}
+                </option>
+              )
+            )}
           </C.Select>
           {errors.category && <C.Error>{errors.category}</C.Error>}
         </C.InputContent>
@@ -139,7 +241,40 @@ const Form = ({ handleAdd, transactionsList, setTransactionsList }) => {
         <C.Button onClick={handleSave}>
           <FontAwesomeIcon icon={faPlus} size="2x" />
         </C.Button>
+
+        <C.Button title="Gere um gráfico das suas despesas" onClick={() => setShowModal(true)}>
+          <FontAwesomeIcon icon={faChartPie} size="2x" />
+        </C.Button>
+        
       </C.Container>
+
+      {showModal && (
+        <C.ModalOverlay>
+          <C.ModalContent>
+            <C.CloseButton onClick={() => setShowModal(false)}>✖</C.CloseButton>
+            <h2>Relatório de Despesas</h2>
+            <div
+              style={{
+                width: "100%",
+                maxWidth: "700px",
+                height: "400px",
+                margin: "20px auto",
+              }}
+            >
+              {generatePieChartData() ? (
+                <Pie
+                  data={generatePieChartData()}
+                  options={{ responsive: true, maintainAspectRatio: false }}
+                />
+              ) : (
+                <p style={{ textAlign: "center", color: "red" }}>
+                  Nenhuma despesa cadastrada.
+                </p>
+              )}
+            </div>
+          </C.ModalContent>
+        </C.ModalOverlay>
+      )}
 
       <Grid itens={transactionsList} setItens={setTransactionsList} />
     </>
--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -58,6 +58,16 @@ export const Button = styled.button`
   background-color: #003366;
 `;
 
+export const ButtonChart = styled.button`
+  padding: 0px 15px;
+  border: none;
+  border-radius: 15px;
+  cursor: pointer;
+  color: white;
+  font-weight: bold;
+  background-color: #003366;
+`;
+
 export const Select = styled.select`
   outline: none;
   border-radius: 5px;
@@ -82,3 +92,58 @@ export const Error = styled.span`
   margin-top: 5px;
   display: block;
 `;
+
+export const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); /* Fundo escuro semi-transparente */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 9999; /* Aparece acima de todo o conteúdo */
+`;
+
+export const ModalContent = styled.div`
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+  width: 90%;
+  max-width: 600px;
+  text-align: center;
+  position: relative; /* Necessário para posicionar o botão "X" corretamente */
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3);
+  animation: fadeIn 0.3s ease-in-out;
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+      transform: scale(0.9);
+    }
+    to {
+      opacity: 1;
+      transform: scale(1);
+    }
+  }
+`;
+
+export const CloseButton = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 15px;
+  background: none;
+  border: none;
+  font-size: 20px;
+  font-weight: bold;
+  cursor: pointer;
+  color: black; /* Cor preta */
+  transition: 0.2s ease-in-out;
+
+  &:hover {
+    color: gray;
+  }
+`;
+
+
